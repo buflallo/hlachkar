a25f16965af6fdbd770defbf3284c9c210920ee7
buflallo <hamza.lachkar@usmba.ac.ma>
2025-08-12 21:30:00 +0200

    app api

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify(status="ok", message="v2")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
